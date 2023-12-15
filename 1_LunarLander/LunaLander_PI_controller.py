43e8dd14ca602d83fb08a51d34d6ce43e0d25e39
YT04KA <ytka@vivaldi.net>
2023-12-15 14:22:32 +0300

    one

--- 1_LunarLander/LunaLander_PI_controller.py
+++ 1_LunarLander/LunaLander_PI_controller.py
@@ -3,36 +3,34 @@ from gymnasium.wrappers.monitoring.video_recorder import VideoRecorder
 import numpy as np
 
 
-def pid(state, params):
+def pi(state, params):
     """
-    расчет управляющего воздействия на основе ПД-регуляторования
+    расчет управляющего воздействия на основе ПИ-регуляторования
     :param state: состояния ОУ
-    :param params: параметры ПД-регуляторов
+    :param params: параметры ПИ-регуляторов
     :return: управляющее воздействие
     """
 
-    # Коэффициенты ПД-регулятора
-    kp_alt = params[0]  # пропорциональная состовляющая по x
-    kd_alt = params[1]  # дифференцирующая состовляющая по x
-    kp_ang = params[2]  # пропорциональная состовляющая по углу
-    kd_ang = params[3]  # дифференцирующая состовляющая по углу
+    # Коэффициенты ПИ-регулятора
+    kp_alt = params[0]  # пропорциональная составляющая по x
+    ki_alt = params[1]  # интегральная составляющая по x
+    kp_ang = params[2]  # пропорциональная составляющая по углу
+    ki_ang = params[3]  # интегральная составляющая по углу
 
-    # расчет целевой переменной
+    # Расчет целевой переменной
     alt_tgt = np.abs(state[0])
-    ang_tgt = (.25 * np.pi) * (state[0] + state[2])
+    ang_tgt = (0.25 * np.pi) * (state[0] + state[2])
 
-    # расчет ошибки
+    # Расчет ошибки
     alt_error = (alt_tgt - state[1])
     ang_error = (ang_tgt - state[4])
 
-    # Формируем управляющее воздействие ПД-регулятора
-    alt_adj = kp_alt * alt_error + kd_alt * state[3]
-    ang_adj = kp_ang * ang_error + kd_ang * state[5]
-
-
-    # Приводим к интервалу (-1,  1)
+    # Формируем управляющее воздействие ПИ-регулятора
+    alt_adj = kp_alt * alt_error + ki_alt * np.cumsum(alt_error)[0]
+    ang_adj = kp_ang * ang_error + ki_ang * np.cumsum(ang_error)[0]
+    # Приводим к интервалу (-1, 1)
     a = np.array([alt_adj, ang_adj])
-    a = np.clip(a, -1, +1)
+    a = np.clip(a, -1, 1)
 
     # Если есть точка соприкосновения с землей, то глушим двигатели, никакие действия не пердаем
     if state[6] or state[7]:
@@ -60,14 +58,10 @@ def start_game(environment, params, video_recorder=False):
         # action = env.action_space.sample()
 
         # ПД-регулятор
-        action = pid(state, params)
+        action = pi(state, params)
         state, reward, done, info, _ = environment.step(action)
         total += reward
 
-        # print(state)  # ‘x’: 10 ‘y’: 6.666 ‘vx’: 5
-        # ‘vy’: 7.5 ‘angle’: 1 ‘angular velocity’: 2.5
-
-        # print(reward, done, info, action)
     return total
 
 
@@ -109,17 +103,18 @@ if __name__ == "__main__":
     print('Размер вектора состояния ОУ: ', env.observation_space.shape)
     print('Структура управляющего воздействия', env.action_space)
 
-    optimize_params = True  # True - если хотим подобрать новые параметры
-    params_pd = np.array([0.84827712, -1.55060286, -1.82178159, 0.8182234])
-
+    optimize_params = False  # True - если хотим подобрать новые параметры
+    #params_pd = np.array([3.42449722,-3.13778014,-4.8181207,5.04894321])
+    params_pd = np.array([1.1453902,2.51241178, -4.90449647,  3.5039919])
     if optimize_params:
         score = start_game(env, params_pd, video_recorder=False)
-        for steps in range(100):
+        for steps in range(100000):
             params_pd, score = optimize(params_pd, score, env, steps+1)
             print("Step:", steps, "Score:", score, "Params:", params_pd)
     else:
-        vid = VideoRecorder(env, path=f"random_luna_lander.mp4")
-        params_pd = np.array([0.84827712, -1.55060286, -1.82178159, 0.8182234])
+        vid = VideoRecorder(env, path=f"random_luna_lander_pi.mp4")
+        #params_pd = np.array([3.42449722,-3.13778014,-4.8181207,5.04894321])
+        params_pd = np.array([-0.5678782, 6.94541676, -4.55419996,3.51633315])
         score = start_game(env, params_pd, video_recorder=vid)
 
         vid.close()
--- 1_LunarLander/LunaLander_P_Controller.py
+++ 1_LunarLander/LunaLander_P_Controller.py
@@ -3,36 +3,32 @@ from gymnasium.wrappers.monitoring.video_recorder import VideoRecorder
 import numpy as np
 
 
-def pid(state, params):
+def p(state, params):
     """
-    расчет управляющего воздействия на основе ПД-регуляторования
+    расчет управляющего воздействия на основе ПИ-регуляторования
     :param state: состояния ОУ
-    :param params: параметры ПД-регуляторов
+    :param params: параметры ПИ-регуляторов
     :return: управляющее воздействие
     """
 
-    # Коэффициенты ПД-регулятора
-    kp_alt = params[0]  # пропорциональная состовляющая по x
-    kd_alt = params[1]  # дифференцирующая состовляющая по x
-    kp_ang = params[2]  # пропорциональная состовляющая по углу
-    kd_ang = params[3]  # дифференцирующая состовляющая по углу
+    # Коэффициенты ПИ-регулятора
+    kp_alt = params[0]  # пропорциональная составляющая по x
+    kp_ang = params[1]  # пропорциональная составляющая по углу
 
-    # расчет целевой переменной
+    # Расчет целевой переменной
     alt_tgt = np.abs(state[0])
-    ang_tgt = (.25 * np.pi) * (state[0] + state[2])
+    ang_tgt = (0.25 * np.pi) * (state[0] + state[2])
 
-    # расчет ошибки
+    # Расчет ошибки
     alt_error = (alt_tgt - state[1])
     ang_error = (ang_tgt - state[4])
 
-    # Формируем управляющее воздействие ПД-регулятора
-    alt_adj = kp_alt * alt_error + kd_alt * state[3]
-    ang_adj = kp_ang * ang_error + kd_ang * state[5]
-
-
-    # Приводим к интервалу (-1,  1)
+    # Формируем управляющее воздействие П-регулятора
+    alt_adj = kp_alt * alt_error
+    ang_adj = kp_ang * ang_error
+    # Приводим к интервалу (-1, 1)
     a = np.array([alt_adj, ang_adj])
-    a = np.clip(a, -1, +1)
+    a = np.clip(a, -1, 1)
 
     # Если есть точка соприкосновения с землей, то глушим двигатели, никакие действия не пердаем
     if state[6] or state[7]:
@@ -60,14 +56,10 @@ def start_game(environment, params, video_recorder=False):
         # action = env.action_space.sample()
 
         # ПД-регулятор
-        action = pid(state, params)
+        action = p(state, params)
         state, reward, done, info, _ = environment.step(action)
         total += reward
 
-        # print(state)  # ‘x’: 10 ‘y’: 6.666 ‘vx’: 5
-        # ‘vy’: 7.5 ‘angle’: 1 ‘angular velocity’: 2.5
-
-        # print(reward, done, info, action)
     return total
 
 
@@ -109,17 +101,16 @@ if __name__ == "__main__":
     print('Размер вектора состояния ОУ: ', env.observation_space.shape)
     print('Структура управляющего воздействия', env.action_space)
 
-    optimize_params = True  # True - если хотим подобрать новые параметры
-    params_pd = np.array([0.84827712, -1.55060286, -1.82178159, 0.8182234])
-
+    optimize_params = False  # True - если хотим подобрать новые параметры
+    params_pd = np.array([0.2000063,   1.28695101, -6.2217881,   3.79098618])
     if optimize_params:
         score = start_game(env, params_pd, video_recorder=False)
-        for steps in range(100):
+        for steps in range(100000):
             params_pd, score = optimize(params_pd, score, env, steps+1)
             print("Step:", steps, "Score:", score, "Params:", params_pd)
     else:
-        vid = VideoRecorder(env, path=f"random_luna_lander.mp4")
-        params_pd = np.array([0.84827712, -1.55060286, -1.82178159, 0.8182234])
+        vid = VideoRecorder(env, path=f"random_luna_lander_p.mp4")
+        params_pd = np.array([0.22946775, -0.81915353, -8.3798925,   2.44939531])
         score = start_game(env, params_pd, video_recorder=vid)
 
         vid.close()
